2123913e7cd4685c8d0199d892f7e0348ebb8ac8
zzh <916672599@qq.com>
2024-05-03 20:30:34 +0800

    学习的新内容

--- src/com/zzh/Thread/ThreadDemo2.java
+++ src/com/zzh/Thread/ThreadDemo2.java
@@ -5,3 +5,21 @@ public class ThreadDemo2 {
 
     }
 }
+class MyRunable1 implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("我在跳");
+        }
+    }
+}
+class MyRunable2 implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("我在跑");
+        }
+    }
+}
